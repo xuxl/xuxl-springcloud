9503bdb0dee72a2bb946b858fd310cce1a644887
xuxiaolong <xu.xiaolong@belle.com.cn>
2019-06-11 14:14:45 +0800

    引入配置中心客户端和配置文件即时更新

--- xuxl-eureka-client/src/main/java/com/xuxl/eureka/client/controller/TestController.java
+++ xuxl-eureka-client/src/main/java/com/xuxl/eureka/client/controller/TestController.java
@@ -5,19 +5,25 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
 
 	Logger logger = LoggerFactory.getLogger(TestController.class);
 	
 	@Autowired
 	DiscoveryClient discoveryClient;
+	
+	@Value("${myname}")
+	private String myname;
 	
 	@GetMapping("/dc")
 	public String dc(){
@@ -44,5 +50,10 @@ public class TestController {
 	public String stop() throws InterruptedException{
 		Thread.sleep(5000L);
 		return "stop";
+	}
+	
+	@GetMapping("/testConfigClient")
+	public String testConfigClient(){
+		return myname;
 	}
 }
